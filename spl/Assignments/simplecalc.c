cc03636dcce6d398a427b9e325f7aef3facefae8
rafidalhaque <68812773+rafidalhaque@users.noreply.github.com>
2024-04-19 15:26:26 +0600

    Upload Assignment of SPL

---
+++ spl/Assignments/simplecalc.c
@@ -0,0 +1,22 @@
+/*
+Assignment: Write A Program in C to calculate result of addition, 
+            subtraction, multiplication, division and modulas operation of 
+            two integer values
+*/
+// By, Rafid Al Haque (315232013)
+
+#include <stdio.h>
+
+int main(){
+    int a= 20,b= 30;
+
+    printf("addition of Two Numbers: %d\n", a+b);
+    printf("subtraction of Two Numbers: %d\n", b-a);
+    printf("multiplication of Two Numbers: %d\n", a*b);
+    printf("division of Two Numbers: %d\n", b/a);
+    printf("odulas operation of Two Numbers: %d\n", b%a);
+
+    return 0;  
+}
+
+/*Output Printed On Next Page*/
